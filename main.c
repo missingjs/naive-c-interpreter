a4b64e10a3eef98a278177b2c259443fc0e0c56c
Qiming Tang <qiming.tang@tusen.ai>
2023-02-22 19:25:53 +0800

    some instructions

--- main.c
+++ main.c
@@ -8,6 +8,18 @@ char *src, *old_src;  // pointer to source code string;
 int poolsize;         // default size of text/data/stack
 int line;             // line number
 
+int *text,            // text segment
+    *old_text,        // for dump text segment
+    *stack;           // stack
+char *data;           // data segment
+
+int *pc, *bp, *sp, ax, cycle; // virtual machine registers
+
+// instructions
+enum { LEA ,IMM ,JMP ,CALL,JZ  ,JNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PUSH,
+       OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,
+       OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT };
+
 void next() {
     token = *src++;
 }
@@ -24,6 +36,19 @@ void program() {
 }
 
 int eval() {  // do nothing yet
+    int op, *tmp;
+    while (1) {
+        if (op == IMM)       {ax = *pc++;}                                     // load immediate value to ax
+        else if (op == LC)   {ax = *(char *)ax;}                               // load character to ax, address in ax
+        else if (op == LI)   {ax = *(int *)ax;}                                // load integer to ax, address in ax
+        // else if (op == SC)   {ax = *(char *)*sp++ = ax;}                       // save character to address, value in ax, address on stack
+        else if (op == SC)   {*(char *)*sp++ = ax;}                            // save character to address, value in ax, address on stack
+        else if (op == SI)   {*(int *)*sp++ = ax;}                             // save integer to address, value in ax, address on stack
+        else if (op == PUSH) {*--sp = ax;}                                     // push the value of ax onto the stack
+        else if (op == JMP)  {pc = (int *)*pc;}                                // jump to the address
+        else if (op == JZ)   {pc = ax ? pc + 1 : (int *)*pc;}                   // jump if ax is zero
+        else if (op == JNZ)  {pc = ax ? (int *)*pc : pc + 1;}                   // jump if ax is not zero
+    }
     return 0;
 }
 
@@ -54,6 +79,27 @@ int main(int argc, char **argv)
     }
     src[i] = 0; // add EOF character
     close(fd);
+
+    // allocate memory for virtual machine
+    if (!(text = old_text = malloc(poolsize))) {
+        printf("could not malloc(%d) for text area\n", poolsize);
+        return -1;
+    }
+    if (!(data = malloc(poolsize))) {
+        printf("could not malloc(%d) for data area\n", poolsize);
+        return -1;
+    }
+    if (!(stack = malloc(poolsize))) {
+        printf("could not malloc(%d) for stack area\n", poolsize);
+        return -1;
+    }
+
+    memset(text, 0, poolsize);
+    memset(data, 0, poolsize);
+    memset(stack, 0, poolsize);
+
+    bp = sp = (int *)((int)stack + poolsize);
+    ax = 0;
 
     program();
     return eval();
